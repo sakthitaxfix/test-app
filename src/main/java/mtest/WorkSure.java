5135361f4ea9afe91eb6a1cd119ea82efdab9c9b
Sakthi Ganesh Arumugasami <sakthiganesharumugasami@Admins-MacBook-Pro.local>
2018-03-30 23:20:41 +0200

    fix

--- src/main/java/mtest/WorkSure.java
+++ src/main/java/mtest/WorkSure.java
@@ -5,7 +5,6 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -13,11 +12,11 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class TestMe {
+public class WorkSure {
 	
 
 	@Test
-	public  void  test() throws MalformedURLException {
+	public  void  pureThoughts() throws MalformedURLException {
 		
 		 DesiredCapabilities capabilities = new DesiredCapabilities();
 		    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
@@ -25,9 +24,9 @@ public class TestMe {
 		    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.2");
 		    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
 		    capabilities.setCapability(MobileCapabilityType.APP, "/Users/sakthiganesharumugasami/Documents/appbuild/Taxfix.app");
-		    WebDriver driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		    WebElement element = driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"Start your tax return\"])[2]"));
-			element.click();
+		    WebDriver driver = new IOSDriver<MobileElement>(new URL("http://192.168.0.18:4723/wd/hub"), capabilities);
+		    MobileElement element = (MobileElement) driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"Start your tax return\"])[2]"));
+		    element.click();
 			driver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"Not now\"]")).click();
 			driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"2015 Begin\"])[2]")).click();
 
