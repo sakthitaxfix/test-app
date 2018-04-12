65f037454d5a5ffd133586cddb93a1a6f1a605c9
Sakthi Ganesh Arumugasami <sakthiganesharumugasami@Admins-MBP-2.fritz.box>
2018-04-12 15:45:21 +0200

    bitrise

--- src/main/java/mtest/TestCircleCI.java
+++ src/main/java/mtest/TestCircleCI.java
@@ -10,7 +10,6 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class TestCircleCI {
 	
@@ -19,16 +18,17 @@ public class TestCircleCI {
 	public  void  pureThoughts() throws MalformedURLException {
 		
 		 DesiredCapabilities capabilities = new DesiredCapabilities();
-		    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
-		    capabilities.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.7.2");
-		    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.2");
-		    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
-		    capabilities.setCapability(MobileCapabilityType.APP, "/Users/sakthiganesharumugasami/Documents/appbuild/Taxfix.app");
-		    WebDriver driver = new IOSDriver<MobileElement>(new URL("http://306e6ea0.ngrok.io/wd/hub"), capabilities);
-		    MobileElement element = (MobileElement) driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"Start your tax return\"])[2]"));
-		    element.click();
-			driver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"Not now\"]")).click();
-			driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"2015 Begin\"])[2]")).click();
+//		    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
+//		    capabilities.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.7.2");
+//		    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.2");
+//		    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
+//		    capabilities.setCapability(MobileCapabilityType.APP, "/Users/sakthiganesharumugasami/Documents/appbuild/Taxfix.app");
+		    WebDriver driver = new IOSDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), capabilities);
+		    driver.findElement(By.xpath("//*[@text='Start your tax return']")).click();
+//		    MobileElement element = (MobileElement) driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"Start your tax return\"])[2]"));
+//		    element.click();
+//			driver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"Not now\"]")).click();
+//			driver.findElement(By.xpath("(//XCUIElementTypeOther[@name=\"2015 Begin\"])[2]")).click();
 
 	}
 
